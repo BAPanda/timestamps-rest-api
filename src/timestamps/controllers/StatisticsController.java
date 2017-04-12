891a35c4df86e708a80c40d8001950b5b785cd5b
Panda <stasabel@gmail.com>
2017-04-12 22:02:18 +0300

    configs for collect-servlet

--- src/timestamps/controllers/StatisticsController.java
+++ src/timestamps/controllers/StatisticsController.java
@@ -7,6 +7,7 @@ import java.math.BigInteger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import timestamps.kafkaintegration.Producer;
@@ -14,10 +15,11 @@ import timestamps.models.Statistics;
 
 @Controller
 
+@RequestMapping("/collector")
 
 public class StatisticsController {
 
-	@RequestMapping("/collector")
+	@RequestMapping(method = RequestMethod.GET)
 	public String printHello(@RequestParam("entId") BigInteger entId, @RequestParam("timestamp") long timestamp,
 			@RequestParam("temper") double temper, @RequestParam("space") double space, @RequestParam("ram") double ram,
 			ModelMap model) throws FileNotFoundException, IOException {
