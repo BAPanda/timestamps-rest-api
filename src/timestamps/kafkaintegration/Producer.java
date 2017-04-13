de6d7acf42bf08171b84c8ce7182b3a4c37b3350
Panda <stasabel@gmail.com>
2017-04-13 13:45:51 +0300

    Kafka connection configs are comleted

--- src/timestamps/kafkaintegration/Producer.java
+++ src/timestamps/kafkaintegration/Producer.java
@@ -1,7 +1,5 @@
 package timestamps.kafkaintegration;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -9,18 +7,25 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 
 import timestamps.models.Statistics;
 
+@PropertySource(encoding = "UTF-8", value = {"classpath:kafkaproducer.properties"})
 public class Producer {
 	
-	private KafkaProducer<BigInteger, String> producer;
+	private KafkaProducer<String, String> producer;
+	
 	private String topic;
 	
 	public Producer() throws FileNotFoundException, IOException {
 		
 		Properties props = new Properties();
-		props.load(new FileInputStream(new File("kafkaproducer.properties")));
+		Resource resource = new ClassPathResource("kafkaproducer.properties");
+		props.load(resource.getInputStream());
 		
 		this.topic = props.getProperty("topic");
 		
@@ -32,7 +37,7 @@ public class Producer {
 	
 	public void send(Statistics stat){
 		
-		producer.send(new ProducerRecord<BigInteger, String>(topic, stat.getEntityID(), stat.toString()));
+		producer.send(new ProducerRecord<String, String>(topic, stat.getEntityID().toString(), stat.toString()));
 		
 	}
 
